908f91f92637deb88e4aea5a088d10f0bf20722c
isso <issoisso3@gmail.com>
2020-03-20 20:18:15 +0900

    routing修正

--- .history/config/routes_20200320200925.rb
+++ .history/config/routes_20200320200925.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
 
-  resources :products, only: [:new, :create ]
-  resources :users, only: [:index]
+  resources :products, :users, only: [:new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
