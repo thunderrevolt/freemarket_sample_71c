906029344b3517f27db64cc8a6a50381e1b0f64c
isso <issoisso3@gmail.com>
2020-03-20 18:58:46 +0900

    registration

--- .history/config/routes_20200320185244.rb
+++ .history/config/routes_20200320185244.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  get '/index', to: 'users#index'
+  get '/index' to: 'users#index'
 
   resources :products, only: [:new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
