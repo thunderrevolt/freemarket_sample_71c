0ee9443ee5d84a4261ae10b24b8fa24ae2f081dc
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-03-23 16:56:21 +0900

    create-mypage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :products, only: [:new, :create ]
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
