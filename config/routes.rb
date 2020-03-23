cba5b46e4d4a90dd86c276cc8840c30a250a0ed6
maru7500 <appleapple7500>
2020-03-23 21:51:53 +0900

    view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-
-  resources :products, only: [:new, :create ]
+  get "/new", to: "products#new"
+  resources :products, only: [:create ]
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
