f4107cf18efc3871af7419136466170e42b45890
sakakibara88 <61224740+sakakibara88@users.noreply.github.com>
2020-03-23 20:25:21 +0900

    .history削除

--- .history/config/routes_20200320182226.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'products#index'
-
-  resources :products, only: [:new, :create ]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
--- .history/config/routes_20200320185306.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'products#index'
-  get '/index', to: 'users#index'
-
-  resources :products, only: [:new, :create ]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
