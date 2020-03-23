e751a850a5b31fcb01571b0a669a2a534ddadeb6
isso <issoisso3@gmail.com>
2020-03-23 16:04:49 +0900

    Revert "新規登録1"
    This reverts commit ce919062f1e34d697de44259d6ae3d3b16e5f1d3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: :production?
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   
@@ -14,12 +13,6 @@ class ApplicationController < ActionController::Base
 
   def production?
     Rails.env.production?
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :tel])
   end
   
 end
