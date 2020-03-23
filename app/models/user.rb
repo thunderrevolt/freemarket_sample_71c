95a615a418d63cc4c6e603d886e480e1c17cb548
isso <issoisso3@gmail.com>
2020-03-23 16:16:01 +0900

    新規登録1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   has_many :products
+  validates :nickname, :first_name, :last_name,
+            :first_name_kana, :last_name_kana,
+            :birthday, :tel, presence: true
 end
