8eb83fc9acc947e5e5e5bb634fef2b52e8065790
isso <issoisso3@gmail.com>
2020-03-23 17:10:37 +0900

    DB_adress1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
-  has_many :products, :address
+  has_many :products
   validates :nickname, :first_name, :last_name,
             :first_name_kana, :last_name_kana,
             :birthday, :tel, presence: true
+  has_one :address
 end
