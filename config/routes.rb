03b6dffa0f90a15767290e02b10dac39b603e612
thunderrevolt <61190252+thunderrevolt@users.noreply.github.com>
2020-03-24 14:24:24 +0900

    add-product-show-view6

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
   resources :products, only: [:new, :create, :show ] do
     resources :comments, only: :create
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/new", to: "products#new"
 
-  resources :products, only: [:new, :create ]
   resources :users, only: [:index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
