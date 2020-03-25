f5bf97c420b719742b1bfa63256873a40fb42c43
isso <issoisso3@gmail.com>
2020-03-25 12:13:09 +0900

    Revert "wizard"
    This reverts commit 44507f55a0f76722d2dd03e1174ba17e67306192.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-  devise_scope :user do
-    get 'addresses', to: 'users/registrations#new_address'
-    post 'addresses', to: 'users/registrations#create_address'
-  end
+  devise_for :users
   root 'products#index'
 
   resources :products, only: [:new, :create ]
