392a3bc9ffaa4a34c95ab37e8bf0b797c54d1591
maru7500 <appleapple7500>
2020-03-31 16:11:05 +0900

    creditcard_test

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    id              {"1"}
+    # nickname        {"1"}
+    # first_name      {"田中"}
+    # last_name       {"太郎"}
+    # first_name_kana {"たなか"}
+    # last_name_kana  {"たろう"}
+    # birthday        {"20010202"}
+    # tel             {"09099990000"}
+    # password        {"00000000"}
+    # email           {"00000000"}
+  end
+end
--- spec/models/creditcard_spec.rb
+++ spec/models/creditcard_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe Creditcard do
+
   describe '#create' do
     it "is invalid without payjp_id" do
-     creditcard = Creditcard.new(user_id: "1", payjp_id: "")
+     creditcard = build(:creditcard, payjp_id: "")
      creditcard.valid?
      expect(creditcard.errors[:payjp_id]).to include("を入力してください")
     #  エラー文が日本語表記になっている。デフォルトは英語で『can't be blank』
@@ -12,9 +13,16 @@ describe Creditcard do
 
   describe '#create' do
     it "is invalid without user_id" do
-     creditcard = Creditcard.new(user_id: "", payjp_id: "aaaaaaaaaaaaaaaaaa")
+     creditcard = build(:creditcard, user_id:"")
      creditcard.valid?
      expect(creditcard.errors[:user_id]).to include("を入力してください")
+    end
+  end
+
+  describe '#create' do
+    it "is valid with a user_id, payjp_id" do
+      card = build(:creditcard)
+      expect(card).to be_valid
     end
   end
 end
