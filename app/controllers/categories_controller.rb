f599539423160d7c0d70a57fad457f84d90beffd
sakakibara88 <61224740+sakakibara88@users.noreply.github.com>
2020-04-02 11:13:23 +0900

    カテゴリー一覧詳細と商品一覧

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,27 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @products = []
     @category = Category.find(params[:id])
+
+    # カテゴリーの孫でなければ！
+    unless @category.children == []
+      @category_children = @category.children
+
+      @category_children.each do |child|
+        # カテゴリーが子なら！
+        if child.children == []
+          @products << Product.where(category_id: child[:id])
+        else # カテゴリーが親なら
+          child.children.each do |grand_child|
+            @products << Product.where(category_id: grand_child[:id])
+          end
+        end
+      end
+    else
+      # カテゴリーが孫なら！
+      @products << Product.where(category_id: @category[:id])
+    end
   end
+
 end
